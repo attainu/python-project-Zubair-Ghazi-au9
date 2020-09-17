e5cf7f0384c8f9d95e60ffb815b4b7c8f270794c
Zubair-Ghazi-a09 <zubairghazi2@gmail.com>
2020-09-17 11:58:35 +0530

    added organize_size

--- fileorganizer.py
+++ fileorganizer.py
@@ -36,7 +36,16 @@ FILE_FORMATS = {file_format: directory
                 for file_format in file_formats}
 
 
-def organize_by_ext():
+def _folder(file_path, parent_folder, folder, each_file):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    shutil.copy2(file_path, folder)
+    destination = os.path.join(folder, each_file)
+    if os.path.exists(destination):
+        os.remove(file_path)
+
+
+def organize_ext():
     input_file_path = os.getcwd()
     for each_file in os.scandir():
         if not each_file.is_dir():
@@ -48,13 +57,39 @@ def organize_by_ext():
                     os.mkdir(parent_folder)
                 folder = os.path.join(parent_folder,
                                       FILE_FORMATS[file_extension])
-                if not os.path.exists(folder):
-                    os.mkdir(folder)
-                shutil.copy2(file_path, folder)
-                destination = os.path.join(folder, each_file)
-                if os.path.exists(destination):
-                    os.remove(file_path)
+                _folder(file_path, parent_folder, folder, each_file)
+
+
+def organize_size():
+    input_file_path = os.getcwd()
+    for each_file in os.scandir():
+        if not each_file.is_dir():
+            file_path = os.path.abspath(each_file)
+            size = os.stat(each_file).st_size
+            parent_folder = os.path.join(input_file_path, "Organized")
+            if not os.path.exists(parent_folder):
+                os.mkdir(parent_folder)
+
+            if 0 <= size < 1000:
+                folder = os.path.join(parent_folder, "Bytes")
+                _folder(file_path, parent_folder, folder, each_file)
+
+            elif 1000 < size < 1000000:
+                folder = os.path.join(parent_folder, "KB")
+                _folder(file_path, parent_folder, folder, each_file)
+
+            elif 1000000 < size < 100000000:
+                folder = os.path.join(parent_folder, "100 MB")
+                _folder(file_path, parent_folder, folder, each_file)
+
+            elif 100000000 < size < 500000000:
+                folder = os.path.join(parent_folder, "500 MB")
+                _folder(file_path, parent_folder, folder, each_file)
+
+            elif size > 1000000000:
+                folder = os.path.join(parent_folder, "GB")
+                _folder(file_path, parent_folder, folder, each_file)
 
 
 if __name__ == "__main__":
-    organize_by_ext()
+    organize_size()
